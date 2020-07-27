4223b324b5a14ed120fd717d76391cce2283b311
Roy Touw <roytouw@hotmail.com>
2020-07-27 10:51:09 +0200

    generic type added to compiler helper.

--- compiler_helper.ts
+++ compiler_helper.ts
@@ -1,5 +1,6 @@
 import * as ts from 'typescript';
 
+// Find the desired node in compiled sourcefile.
 const fetchNodeFromSourceFile = (node: ts.SourceFile | ts.Node): ts.Node => {
     if (ts.isSourceFile(node) || node.kind === 312 || ts.isExpressionStatement(node)) {
         return fetchNodeFromSourceFile(node.getChildren().shift());
@@ -8,12 +9,15 @@ const fetchNodeFromSourceFile = (node: ts.SourceFile | ts.Node): ts.Node => {
     }
 };
 
+type Test = ts.Node;
+
 // Create ts.Node from given typescript code.
-export const createNode = (code: string, type: number): ts.Node => {
+export const createNode = <T extends ts.Node>(code: string, type: number): T => {
     const sourcefile = ts.createSourceFile('test.ts', code, ts.ScriptTarget.ES2015, true);
     const node = fetchNodeFromSourceFile(sourcefile);
     if (node.kind !== type) {
         throw new Error(`TypeScript node created from given code doesn't match expected type, expected: ${type} but compiled: ${node.kind}!`);
     }
-    return node;
+
+    return node as T;
 };
